5ade4445b90422e547ce6b064a9e88d492db7879
Helton Pereira <helton.bfsm@gmail.com>
2022-05-26 14:55:24 -0300

    Requisito 4 aparentemente cumprido

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const color = document.getElementsByClassName('color');
+let pixelBoard = document.getElementById('pixel-board');
 
 for (index = 0; index < color.length; index += 1) {
-    if (color[index].innerHTML === 'Preto') {
-        color[index].style.background = 'black';
-    } else if (color[index].innerHTML === 'Amarelo') {
-        color[index].style.background = 'yellow'; 
-    } else if (color[index].innerHTML === 'Vermelho') {
-        color[index].style.background = 'red';
-    } else if (color[index].innerHTML === 'Azul') {
-        color[index].style.background = 'blue';
-    }
+  if (color[index] === color[0]) {
+    color[index].style.background = 'black';
+  } else if (color[index] === color[1]) {
+    color[index].style.background = 'yellow';
+  } else if (color[index] === color[2]) {
+    color[index].style.background = 'red';
+  } else if (color[index] === color[3]) {
+    color[index].style.background = 'blue';
+  }
+}
+
+for (index0 = 0; index0 < 5; index0 += 1) {
+  for (index1 = 0; index1 < 5; index1 += 1) {
+    let boardElement = document.createElement('div');
+    pixelBoard.appendChild(boardElement);
+    boardElement.classList.add('pixel');
+  }
 }
